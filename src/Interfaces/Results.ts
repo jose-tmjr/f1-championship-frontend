45e69d1404f91d359828d29354865d013bf95601
Jose Teixeira <josejonicaro@gmail.com>
2025-03-26 21:28:39 -0300

    refactor: add new attributes to Results Interface

--- src/Interfaces/Results.ts
+++ src/Interfaces/Results.ts
@@ -1,7 +1,9 @@
 export interface Results {
   driver_fastest_lap: string;
   driver_id: number;
-  driver_position: number;
+  driver_start_position: number;
+  driver_final_position: number;
+  grid_result: number;
   race_id: number;
   team_id: number;
 }
