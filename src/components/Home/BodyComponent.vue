fb566b7b5c0f4177ec7d82cf3a41e499acc75ebe
Jose Teixeira <josejonicaro@gmail.com>
2025-03-26 21:29:55 -0300

    feat: update home body component to read DriversModel

--- src/components/Home/BodyComponent.vue
+++ src/components/Home/BodyComponent.vue
@@ -4,20 +4,20 @@
     <table class="w-full text-left border-collapse">
       <thead>
         <tr class="bg-gray-800 text-white">
-          <th class="p-3 border border-gray-700">ID</th>
           <th class="p-3 border border-gray-700">Name</th>
           <th class="p-3 border border-gray-700">Team</th>
+          <th class="p-3 border border-gray-700">Points</th>
         </tr>
       </thead>
       <tbody>
         <tr
           v-for="driver in drivers"
-          :key="driver.driver_id"
+          :key="driver.id"
           class="odd:bg-gray-100 even:bg-gray-200 hover:bg-gray-300 transition-colors"
         >
-          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.driver_id }}</td>
-          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.driver_name }}</td>
-          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.team_id }}</td>
+          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.name }}</td>
+          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.teamName }}</td>
+          <td class="p-3 border border-gray-300 text-gray-900">{{ driver.points }}</td>
         </tr>
       </tbody>
     </table>
@@ -27,13 +27,14 @@
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import HomeService from "@/services/HomeService";
-import type { Driver } from "@/Interfaces/Driver";
+import type DriverModel from "@/models/driverModel";
 
-const drivers = ref<Driver[]>([]);
+const drivers = ref<DriverModel[]>([]);
 
 async function loadDrivers() {
   try {
-    drivers.value = await HomeService.getDrivers();
+    const service = await HomeService.create();
+    drivers.value = await service.getDrivers();
   } catch (error) {
     console.error("Erro ao carregar motoristas:", error);
   }
