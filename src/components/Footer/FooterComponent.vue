de2ad94524f5cc028ae781d24a84c3769665f7f1
Matheus Leodonel <mleodonel@gmail.com>
2026-09-24 00:56:44 -0300

    Ajuste path APIs

--- src/components/Footer/FooterComponent.vue
+++ src/components/Footer/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <div class="wrap">
-            <p class="text-center text-white">© 2025 F1 Standings</p>
+            <p class="text-center text-white">© 2026 F1 Standings</p>
         </div>
     </footer>
 </template>
